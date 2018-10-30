210b4b69b37c1644237255cd631bc911dddb0eed
yyci <yyc12yyc>
2018-10-30 11:11:35 +0800

    1、修改绑定产品文案

--- scrmapp-wechat/src/main/java/com/ziwow/scrmapp/wechat/controller/WechatController.java
+++ scrmapp-wechat/src/main/java/com/ziwow/scrmapp/wechat/controller/WechatController.java
@@ -553,7 +553,7 @@ public class WechatController {
             mobileService.sendContentByEmay(miniappSendSms.getPhone(),msgContent, Constant.BIND_GIFT );
         }else if (miniappSendSms.getType()==4){//注册有礼
 //            String msgContent = "嗨，欢迎进入沁园水健康守护基地，恭喜您已完成会员注册。小沁送您会员尊享立减100元优惠券一张，购买微商城内任意净水器通用哦。进入沁园WX号（qy_serve ），点击<要购买-WX商城>进入<我的>即可查看。立即绑定产品，还有更多滤芯优惠券等着您！即刻开启您的专享优惠通道吧~回复TD退订";
-            String msgContent = "嗨，欢迎进入沁园水健康守护基地，恭喜您已完成会员注册。小沁送您会员尊享立减100元优惠券一张，购买微商城内任意净水器通用哦。进入沁园WX号（qy_serve ），点击<要购买-WX商城>进入<我的>即可查看。即刻开启您的专享优惠通道吧~回复TD退订";
+            String msgContent = "嗨，欢迎进入沁园水健康守护基地，恭喜您已完成会员注册。小沁送您会员尊享立减100元优惠券一张，购买微商城内任意净水器通用哦。进入沁园WX号（qy_serve ），点击<我的沁园>进入<优惠券>即可查看。即刻开启您的专享优惠通道吧~回复TD退订";
             mobileService.sendContentByEmay(miniappSendSms.getPhone(), msgContent,Constant.REGISTER_GIFT);
         }
 
