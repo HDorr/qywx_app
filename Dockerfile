274eef79de677257b91cf07b0d3148955bb6e9b1
huangjie <jitrejake@163.com>
2019-03-28 11:20:21 +0800

    1.dockerfile 2.构建推送脚本

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,16 @@
 #scrm app
-FROM jitre/scrm_env:0.1
+FROM 47.96.110.23:8081/scrm/base:0.01
 LABEL auth="Jitre"
 LABEL time="2019年03月23日"
 
 WORKDIR /root
 
-COPY  scrmapp.war /usr/local/tomcat/webapps/wx.war
+COPY  scrmapp-webapp/target/scrmapp.war /usr/local/tomcat/webapps/wx.war
 
 # 端口开放声明
 EXPOSE 8080
 
 # 启动脚本
 # docker run -d --net=scrm --name=wx -p 8080:8080 -v /opt/ziwow:/opt/ziwow ID 
-
 
 
---
+++ buildImageProductionEnv.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# 1.检查参数的格式
+if [ $# -lt 1 ]
+then
+    echo -e "===================>参数的个数不正确"
+    exit 0
+fi
+
+ENV=production
+TAG=$1
+
+# 2.让用户判断环境和标签的正确性
+echo -e "===================>请确认当前的环境:$ENV,镜像标签:$TAG [Y/N]"
+read answer
+
+if [ "$answer" -eq "N" ]
+then
+    exit 0
+fi
+
+echo -e "===================>打包-scrm-生产环境"
+cd scrmapp-parent
+mvn clean package -P$ENV
+
+# 3.构建镜像
+cd ..
+echo "===================>镜像构建-scrm-测试环境"
+docker login 47.96.110.23:8081
+docker build -t="47.96.110.23:8081/scrm/app:$TAG-$ENV" .
+echo "===================>推送镜像"
+docker push "47.96.110.23:8081/scrm/app:$TAG-$ENV"
+
+
