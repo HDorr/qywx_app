becd066483d0f63ba0a1b2c862709a7eb1c129e8
huangjie <jitrejake@163.com>
2019-04-11 21:44:38 +0800

    1.传递mp3

--- scrmapp-wechat/src/main/java/com/ziwow/scrmapp/wechat/service/impl/WechatMediaServiceImpl.java
+++ scrmapp-wechat/src/main/java/com/ziwow/scrmapp/wechat/service/impl/WechatMediaServiceImpl.java
@@ -158,8 +158,9 @@ public class WechatMediaServiceImpl implements WechatMediaService,InitializingBe
         }
       }
       //生成对应mp3格式
-      ChangeAudioFormat.changeToMp3(filePath, picPath + picName + ".mp3");
-      return  filePath;
+      String mp3File=picPath+picName+".mp3";
+      ChangeAudioFormat.changeToMp3(filePath, mp3File);
+      return  mp3File;
     }
   }
 
