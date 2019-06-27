e81a9ccd41078266ea0f5c91f698deff7a59d885
jackzhu <ulovecode@gmail.com>
2019-06-27 19:11:38 +0800

    调整vo

--- scrmapp-common/src/main/java/com/ziwow/scrmapp/common/bean/vo/csm/BaseCardVo.java
+++ scrmapp-common/src/main/java/com/ziwow/scrmapp/common/bean/vo/csm/BaseCardVo.java
@@ -13,7 +13,18 @@ public class BaseCardVo implements Serializable {
      */
     private Status status;
 
+    /**
+     * 返回数据
+     */
+    private String data;
 
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
 
     public Status getStatus() {
         return status;
