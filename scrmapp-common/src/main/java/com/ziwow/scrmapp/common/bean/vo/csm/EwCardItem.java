75d1efd18cd6deb40eaf2788aaf29a8046fa4bfb
jackzhu <ulovecode@gmail.com>
2019-06-27 18:23:01 +0800

    查询延保卡vo调整

--- scrmapp-common/src/main/java/com/ziwow/scrmapp/common/bean/vo/csm/EwCardItem.java
+++ scrmapp-common/src/main/java/com/ziwow/scrmapp/common/bean/vo/csm/EwCardItem.java
@@ -1,7 +1,6 @@
 package com.ziwow.scrmapp.common.bean.vo.csm;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.Date;
@@ -11,9 +10,26 @@ import java.util.Date;
  * @author songkaiqi
  * @since 2019/06/09/下午2:38
  */
-@JsonInclude(JsonInclude.Include.NON_NULL)
 public class EwCardItem {
 
+    /**
+     * 卡号
+     */
+    @JsonProperty("card_no")
+    private String cardNo;
+
+    /**
+     * 使用状态
+     */
+    @JsonProperty("card_stat")
+    private String cardStat;
+
+    /**
+     * 购买时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+    @JsonProperty("purch_date")
+    private Date purchDate;
 
     /**
      * 对应机型
@@ -21,45 +37,52 @@ public class EwCardItem {
     @JsonProperty("item_name")
     private String itemName;
 
-
-    /**
-     * 对应编码
-     */
-    @JsonProperty("item_code")
-    private String itemCode;
-
-
     /**
      * 延保期限
      */
     @JsonProperty("valid_time")
     private int validTime;
 
-
     /**
-     * 获取拆分后的结果
-     * @return
+     * 保修期限
      */
-    public String[] getItemNames(){
-        return this.itemName.split(",");
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+    @JsonProperty("repair_term")
+    private Date repairTerm;
+
+
+
+
+    public Date getPurchDate() {
+        return purchDate;
     }
 
-    /**
-     * 获取拆分后的结果
-     * @return
-     */
-    public String[] getItemCodes(){
-        return this.itemCode.split(",");
+    public void setPurchDate(Date purchDate) {
+        this.purchDate = purchDate;
     }
 
-
-
-    public String getItemCode() {
-        return itemCode;
+    public Date getRepairTerm() {
+        return repairTerm;
     }
 
-    public void setItemCode(String itemCode) {
-        this.itemCode = itemCode;
+    public void setRepairTerm(Date repairTerm) {
+        this.repairTerm = repairTerm;
+    }
+
+    public String getCardNo() {
+        return cardNo;
+    }
+
+    public void setCardNo(String cardNo) {
+        this.cardNo = cardNo;
+    }
+
+    public String getCardStat() {
+        return cardStat;
+    }
+
+    public void setCardStat(String cardStat) {
+        this.cardStat = cardStat;
     }
 
     public String getItemName() {
