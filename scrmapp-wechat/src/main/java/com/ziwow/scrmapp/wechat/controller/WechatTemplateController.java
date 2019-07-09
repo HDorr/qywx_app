85ee5a29ac08de8e5559acb5ead3c19fdeaa776a
huangjie <jitrejake@163.com>
2019-07-09 11:58:21 +0800

    1.增加模板接口的路径和跳转小程序控制

--- scrmapp-wechat/src/main/java/com/ziwow/scrmapp/wechat/controller/WechatTemplateController.java
+++ scrmapp-wechat/src/main/java/com/ziwow/scrmapp/wechat/controller/WechatTemplateController.java
@@ -41,7 +41,7 @@ public class WechatTemplateController {
     public Result sendTemplateNotify(@RequestParam("timestamp") String timestamp,
                                          @RequestParam("signture") String signture,
                                          @RequestParam("unionId") String unionId,@RequestParam String type,
-        @RequestParam String param) {
+        @RequestParam String param,String url,Boolean toMni) {
         Result result = new BaseResult();
         try {
             boolean isLegal = SignUtil.checkSignature(signture, timestamp, Constant.AUTH_KEY);
@@ -66,7 +66,7 @@ public class WechatTemplateController {
                 logger.error("发送通知模板出错，用户不存在");
                 return result;
             }
-            wechatTemplateService.sendTemplate(fans.getOpenId(),"", paramList,type, false);
+            wechatTemplateService.sendTemplate(fans.getOpenId(),StringUtils.isNotBlank(url)?url:"", paramList,type, toMni!=null?toMni:false);
         } catch (Exception e) {
             logger.error("通知发送失败，原因", e);
             result.setReturnCode(Constant.FAIL);
