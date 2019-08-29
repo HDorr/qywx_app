9b9ca27dbdcedf34689a639da20ce6b0b0660488
黄5 <jitrejake@gmail.com>
2019-08-29 21:40:23 +0800

    1.修改订单默认送货方式

--- scrmapp-webapp/src/main/webapp/resources/updatesql/update2019-8-29.sql
+++ scrmapp-webapp/src/main/webapp/resources/updatesql/update2019-8-29.sql
@@ -1,5 +1,5 @@
 alter table t_wechat_orders
- add delivery_type enum('NORMAL', 'DEALER') default "ETW" not null comment '发货方式  NORMAL:工厂发货  DEALER:带货上门';
+ add delivery_type enum('NORMAL', 'DEALER') default "NORMAL" not null comment '发货方式  NORMAL:工厂发货  DEALER:带货上门';
 
 alter table t_wechat_orders
  add departmentName varchar(50) null comment '服务网点';
