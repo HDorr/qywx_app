5fdce1d8443d429426c5a0e5f196ed9ec3fd95b7
jackzhu <ulovecode@gmail.com>
2019-09-04 13:58:24 +0800

    修改延保卡短信文案

--- scrmapp-wechat/src/main/java/com/ziwow/scrmapp/wechat/schedule/GrantEwCardTask.java
+++ scrmapp-wechat/src/main/java/com/ziwow/scrmapp/wechat/schedule/GrantEwCardTask.java
@@ -2,6 +2,7 @@ package com.ziwow.scrmapp.wechat.schedule;
 
 import com.xxl.job.core.biz.model.ReturnT;
 import com.xxl.job.core.handler.annotation.JobHandler;
+import com.xxl.job.core.log.XxlJobLogger;
 import com.ziwow.scrmapp.wechat.persistence.entity.GrantEwCardRecord;
 import com.ziwow.scrmapp.wechat.service.GrantEwCardRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,14 +22,37 @@ public class GrantEwCardTask extends AbstractGrantEwCard{
     @Autowired
     private GrantEwCardRecordService grantEwCardRecordService;
 
+    private volatile boolean flag = true;
+
     @Override
     public ReturnT<String> execute(String s) throws Exception {
-        List<GrantEwCardRecord> records = grantEwCardRecordService.selectRecord();
-        for (GrantEwCardRecord record : records) {
-            final boolean flag = grantEwCard(record.getPhone(), record.getType());
-            if (flag){
-                grantEwCardRecordService.updateSendByPhone(record.getPhone(),true);
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                List<GrantEwCardRecord> records = grantEwCardRecordService.selectRecord();
+                for (GrantEwCardRecord record : records) {
+                    if (flag){
+                        final boolean grant = grantEwCard(record.getPhone(), record.getType());
+                        if (grant) {
+                            grantEwCardRecordService.updateSendByPhone(record.getPhone(), true);
+                        }
+                    }else {
+                        XxlJobLogger.log("发放延保卡子任务停止");
+                        break;
+                    }
+                }
             }
+        });
+
+        thread.start();
+
+        try {
+            synchronized (thread) {
+                thread.wait();
+            }
+        } catch (InterruptedException e) {
+            flag = false;
+            XxlJobLogger.log("发放延保卡主任务停止");
         }
         return ReturnT.SUCCESS;
     }
