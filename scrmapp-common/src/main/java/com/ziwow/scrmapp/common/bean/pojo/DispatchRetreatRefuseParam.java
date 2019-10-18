f09248396024bb10766db330ca0ca48b7141279b
jackzhu <ulovecode@gmail.com>
2019-10-18 14:47:49 +0800

    时间增加时区

--- scrmapp-common/src/main/java/com/ziwow/scrmapp/common/bean/pojo/DispatchRetreatRefuseParam.java
+++ scrmapp-common/src/main/java/com/ziwow/scrmapp/common/bean/pojo/DispatchRetreatRefuseParam.java
@@ -26,7 +26,7 @@ public class DispatchRetreatRefuseParam {
     /**
      * 申请时间
      */
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss",timezone = "GMT+8")
     private Date manualDate;
 
     public String getTimeStamp() {
