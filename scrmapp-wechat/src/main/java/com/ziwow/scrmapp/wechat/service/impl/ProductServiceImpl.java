e242a75e15e1890ab54de0cb91d9e15b62ea770a
wd-Aiolos <474105037@qq.com>
2020-01-03 14:34:42 +0800

    绑定机器时间替购买时间,剔除粉丝表save逻辑

--- scrmapp-wechat/src/main/java/com/ziwow/scrmapp/wechat/service/impl/ProductServiceImpl.java
+++ scrmapp-wechat/src/main/java/com/ziwow/scrmapp/wechat/service/impl/ProductServiceImpl.java
@@ -964,8 +964,8 @@ public class ProductServiceImpl implements ProductService {
         Map<String, String> map = null;
         if(CollectionUtils.isNotEmpty(products)){
             int size = products.size();
-            Date first = products.get(0).getCreateTime();
-            Date last = size != 1 ? products.get(size - 1).getCreateTime() : first;
+            Date first = products.get(0).getBuyTime();
+            Date last = size != 1 ? products.get(size - 1).getBuyTime() : first;
             String pd = DateUtil.DateToString(first, DateUtil.YYYY_MM_DD);
             String ld = DateUtil.DateToString(last, DateUtil.YYYY_MM_DD);
             String pn = size != 1 ? products.get(size - 1).getProductName()
